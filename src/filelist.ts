f9c30d25c087e6ee97521f531b09b86cb4606bae
Zhangp <909153789@qq.com>
2018-05-03 19:25:13 +0800

    v0.1.0
    v0.1.0

--- src/filelist.ts
+++ src/filelist.ts
@@ -29,11 +29,27 @@ interface File {
   isDir: boolean;
 }
 
+interface Options {
+  ignore: string[]; // 读取文件的时候忽略的文件和目录
+}
+
 let stack: Array<File> = [];
 
-function main(start: string): string {
+function main(start: string, options?: Options): string {
+  start = path.resolve(__dirname, start);
   let fileContent: string = '';
+  let ignorePath: string[] = [];
   let stats: fs.Stats = fs.statSync(start);
+
+  if (options) {
+    ignorePath = options.ignore.map(ig => {
+      return path.resolve(start, ig)
+    })
+  }
+  if (ignorePath.indexOf(start) > -1) {
+    return fileContent
+  }
+
   if (stats.isFile()) {
     return read(start)()
   }
@@ -43,6 +59,10 @@ function main(start: string): string {
 
     while (stack.length > 0) {
       let node = stack.pop();
+      // 忽略不需要检查的文件
+      if (ignorePath.indexOf(node.path) > -1) {
+        continue
+      }
       if (node.isFile) {
         fileContent += node.read()
       }
@@ -52,7 +72,6 @@ function main(start: string): string {
       }
     }
     return fileContent
-
   }
 }
 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import fileContent from './filelist'
 import path = require('path');
+import fs = require('fs');
+import { fstat } from 'fs';
 
-const START: string = path.resolve(__dirname, '../../work/lecai-vue-trunk/eLearningH5.01/src/components');
+const START: string = path.resolve(__dirname, '../../../work/lecai-vue-trunk/eLearningH5.01/src');
 
-const LANG: string = path.resolve(__dirname, '../../work/lecai-vue-trunk/eLearningH5.01/src/language.js');
+const LANG: string = path.resolve(__dirname, '../../../work/lecai-vue-trunk/eLearningH5.01/src/language.js');
 
 
-let tStr: string = fileContent(START);
+let tStr: string = fileContent(START, {
+  ignore: [
+    'assets',
+    'language.js',
+    'components/view/exam',
+    'components/view/training',
+    'language',
+    'HTone.js'
+  ]
+});
 let lang: string = fileContent(LANG);
 
 interface Lang {
@@ -23,12 +34,53 @@ interface I18n {
 
 let {ch, en, ha} = <I18n>(JSON.parse(lang));
 
-// let shakeKey: string[] = [];
-// for (let i in ch.message) {
-//   if (tStr.indexOf(i) === -1) {
-//     shakeKey.push(i)
-//   }
-// }
+// 保留字段
+const PRESERVEKEY: string[] = [
+  'global.',
+  'apis.',
+  'api.',
+  'posts.',
+  'name.',
+  'face.',
+  'advert.',
+  'apisorg.',
+  'pis.'
+]
 
-// console.log(shakeKey);
-console.log(tStr);
+// 检测字段是不是保留字段
+function checkPreserveKey(key: string): boolean {
+  return PRESERVEKEY.every(preKey => {
+    return key.indexOf(preKey) !== 0
+  })
+}
+
+function search(lang: Lang): Lang {
+  let newLang: Lang = {
+    message: {}
+  }
+  for (let i in lang.message) {
+    if (checkPreserveKey(i) && tStr.indexOf(i) === -1) {
+      // console.log(i)
+    } else {
+      newLang.message[i] = lang.message[i]
+    }
+  }
+  return newLang
+}
+
+function format(newLang: Lang, type: string): string {
+  let str = JSON.stringify(newLang);
+  str = `const locales={${type}:${str}}\nexport {locales}`;
+  return str.replace(/'/g, "\\'").replace(/"/g, "'");
+}
+
+function write(path: string, data: string): void {
+  fs.writeFile(path, data, 'utf-8', err => {
+    if (err) console.log(err);
+    console.log('ok.')
+  })
+}
+
+write('./ch.js', format(search(ch), 'ch'))
+write('./en.js', format(search(en), 'en'))
+write('./ha.js', format(search(ha), 'ha'))
